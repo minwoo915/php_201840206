023b8f0056b7fb4b3fb0d704771604f6f0e482a5
kimminwoo <minwoo915@naver.com>
2019-09-04 16:54:32 +0900

    layout

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$layout = file_get_contents("../theme/index.html".$filename);
+
+$filename = $_SERVER['REQUEST_URI'].".html";
+$body = file_get_contents("../".$filename);
+
+if(file_exists("../.$filename")){
+    $body = file_get_contents("../.$filename");
+    $layout = str_replace("{{main}}",$body,$layout);
+}else{
+    $body = file_get_contents("../home.html");
+    $layout = str_replace("{{main}}",$body,$layout);
+}
+
+echo $layout;
+/*
+echo "내가 처음이야";
+
+$filename = $_SERVER['REQUEST_URI'].".html";
+echo $filename."을 읽어옵니다.";
+
+$body = file_get_contents("../".$filename);
+echo $body;
+*/
